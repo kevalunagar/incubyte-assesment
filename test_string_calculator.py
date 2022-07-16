63d69bedf8838cd1699f96f7e3eec579d71b9dc1
kevalunagar <unagarkeval1@gmail.com>
2022-07-16 12:12:07 +0530

    Assignment Added

---
+++ test_string_calculator.py
@@ -0,0 +1,31 @@
+import unittest
+
+from string_calculator import add
+
+class TestStringCalculator(unittest.TestCase):
+
+  def test_empty_string(self):
+    self.assertEqual(add(""), 0)
+
+  def test_comma(self):
+    self.assertEqual(add("1"), 1)
+    self.assertEqual(add("1,2"), 3)
+
+  def test_newlines(self):
+    self.assertEqual(add("1\n2,3"), 6)
+    self.assertRaises(ValueError, add, "1,\n")
+
+  def test_delimeter(self):
+    self.assertEqual(add("//;\n1;2;3;4"), 10)
+    self.assertEqual(add("//*\n1*2*3*4"), 10)
+    self.assertRaises(ValueError, add, "//*\n1*\n")
+
+  def test_negatives(self):
+    self.assertRaises(ValueError, add ,"1,-2")
+
+    try:
+      add("1,-2,3")
+    except ValueError as e:
+      self.assertEqual(str(e), 'negatives not allowed. found -2')
+
+unittest.main()
