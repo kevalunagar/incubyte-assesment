095125e66e248486badcbdfb390c06bc059fcd2e
Keval Unagar <77386103+kevalunagar@users.noreply.github.com>
2023-01-06 16:59:16 +0530

    Update string_calculator.py

--- string_calculator.py
+++ string_calculator.py
@@ -1,15 +1,16 @@
 import re
 
+# Add Function which takes string of numbers
 def add(numbers: str):
-  if numbers == '':
+  if numbers == '': # if string is empty
     return 0
 
-  del_ = None
+  del_ = None # delimeter
   if numbers[:2] == '//':
     numbers = numbers[2:]
     del_, numbers = re.split('\n', numbers, 1)
 
-  pattern = ',|\n'
+  pattern = ',|\n' # to find patterns
   if del_ is not None:
     pattern = f',|\n|\{del_}'
 
